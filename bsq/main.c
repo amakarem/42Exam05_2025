c45ec18d90a5222d72671b96a5433b14c01561b0
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2026-03-04 21:33:12 +0100

    level 1

--- bsq/main.c
+++ bsq/main.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void free_arr(char **arr, int **d, int rows)
+int error_msg()
 {
-    if (arr)
+    fprintf(stderr, "map error\n");
+    return (1);
+}
+
+void free_all(char **map, int **dp, int rows)
+{
+    if (map)
     {
-        int i = 0;
-        while (i < rows)
-        {
-            free(arr[i]);
-            i++;
-        }
-        free(arr);
+        for (int i = 0; i < rows; i++)
+            free(map[i]);
+        free(map);
     }
-    if (d)
+    if (dp)
     {
-        int i = 0;
-        while (i < rows)
-        {
-            free(d[i]);
-            i++;
-        }
-        free(d);
+        for (int i = 0; i < rows; i++)
+            free(dp[i]);
+        free(dp);
     }
 }
 
-int ext_error(char **map, int **dp, int rows)
+int ext_err(char **map, int **dp, int rows)
 {
-    if (rows > 0)
-        free_arr(map, dp, rows);
-    fprintf(stderr, "map error\n");
-    return (1);
+    free_all(map, dp, rows);
+    return error_msg();
 }
 
 int min(int a, int b, int c)
@@ -43,102 +39,99 @@ int min(int a, int b, int c)
     return m;
 }
 
-int bsq(int input, char **argv)
+int bsq(FILE *f)
 {
     int rows;
     int cols = 0;
+    int read;
+    int **dp = NULL;
+    int best_size = 0, best_r = 0, best_x = 0;
     char empty, obstacle, full;
     char **map = NULL;
-    int **dp = NULL;
-    int best_size = 0, best_x = 0, best_y = 0;
-    FILE *f;
-    if (input > 0)
-        f = fopen(argv[input], "r");
-    else
-        f = stdin;
-    if (!f)
-        return ext_error(map, dp, 0);
+    size_t len = 0;
     if (fscanf(f, "%d %c %c %c\n", &rows, &empty, &obstacle, &full) != 4)
-        return ext_error(map, dp, 0);
+        return error_msg();
     map = malloc(rows * sizeof(char*));
     if (!map)
-        return ext_error(map, dp, 0);
-    size_t len = 0;
-    int read;
+        return error_msg();
     for (int i = 0; i < rows; i++)
     {
         map[i] = NULL;
         read = getline(&map[i], &len, f);
         if (read <= 0)
-            return ext_error(map, dp, rows);
-        if(map[i][read - 1] == '\n' || map[i][read - 1] == '\r')
+            return ext_err(map, dp, rows);
+        if (map[i][read - 1] == '\n' || map[i][read - 1] == '\r')
         {
             map[i][read - 1] = '\0';
-			read--;
-        }
-        else
-            return ext_error(map, dp, rows);
+            read--;
+        } else
+            return ext_err(map, dp, rows);       
         if (i == 0)
             cols = read;
-        else if (read != cols)
-            return ext_error(map, dp, rows);
+        else if (cols != read)
+            return ext_err(map, dp, rows);
     }
-    if (input > 0)
-        fclose(f);
     dp = malloc(rows * sizeof(int*));
     if (!dp)
-        return ext_error(map, dp, rows);
-    for (int i =0; i < rows; i++)
+        return ext_err(map, dp, rows);
+    for (int r = 0; r < rows; r++)
     {
-        dp[i] = calloc(cols, sizeof(int));
-        if (!dp[i])
-            return ext_error(map, dp, rows);
-        for (int j = 0; j < cols; j++)
+        dp[r] = calloc(cols, sizeof(int));
+        if (!dp[r])
+            return ext_err(map, dp, rows);
+        for (int x = 0; x < cols; x++)
         {
-            char c = map[i][j];
+            char c = map[r][x];
             if (c != empty && c != obstacle)
-                return ext_error(map, dp, rows);
+                return ext_err(map, dp, rows);
             if (c == obstacle)
-                dp[i][j] = 0;
-            else if (i == 0 || j == 0)
-                dp[i][j] = 1;
+                dp[r][x] = 0;
+            else if (r == 0 || x == 0)
+                dp[r][x] = 1;
             else
-                dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]);
-            if (dp[i][j] > best_size)
+                dp[r][x] = 1 + min(dp[r - 1][x], dp[r -1][x - 1], dp[r][x - 1]);
+            if (dp[r][x] > best_size)
             {
-                best_size = dp[i][j];
-                best_x = i;
-                best_y = j;
+                best_size = dp[r][x];
+                best_r = r;
+                best_x = x;
             }
         }
     }
-    for (int i = best_x - best_size + 1; i <= best_x; i++)
+    for (int r = best_r - best_size + 1; r <= best_r; r++)
     {
-        for (int j = best_y - best_size + 1; j <= best_y; j++)
-            map[i][j] = full;
+        for (int x = best_x - best_size + 1; x <= best_x; x++)
+            map[r][x] = full;
     }
-    for (int i =0; i < rows; i++)
+    for (int r = 0; r < rows; r++)
     {
-        for (int j = 0; j < cols; j++)
-            putchar(map[i][j]);
+        for (int x = 0; x < cols; x++)
+            putchar(map[r][x]);
         putchar('\n');
     }
-    free_arr(map, dp, rows);
+    free_all(map, dp, rows);
     return 0;
 }
 
 int main(int argc, char **argv)
 {
-    int r = 0;
+    FILE *f;
+    int r;
     if (argc > 1)
     {
-        for (int i = 1; i < argc; i++)
+        for(int i = 1; i < argc; i++)
         {
-            r = bsq(i, argv);
-            if (i < argc - 1)
+            f = fopen(argv[i], "r");
+            if (!f)
+                return error_msg();
+            r = bsq(f);
+            fclose(f);
+            if (i < argc -1)
                 fprintf(stdout, "\n");
         }
-    } else
-        r = bsq(0, argv);
-    return r;
+    }
+    else
+        r = bsq(stdin);
+    
+    return (r);
 }
