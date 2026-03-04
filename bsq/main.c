31f8ba0d2027e29fe191c3c61b198b8368b74383
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2026-03-04 22:27:31 +0100

    level 1

--- bsq/main.c
+++ bsq/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int error_msg()
+int err_msg()
 {
     fprintf(stderr, "map error\n");
     return (1);
@@ -11,13 +11,13 @@ void free_all(char **map, int **dp, int rows)
 {
     if (map)
     {
-        for (int i = 0; i < rows; i++)
+        for (int i =0; i < rows; i++)
             free(map[i]);
         free(map);
     }
     if (dp)
     {
-        for (int i = 0; i < rows; i++)
+        for (int i =0; i < rows; i++)
             free(dp[i]);
         free(dp);
     }
@@ -26,7 +26,7 @@ void free_all(char **map, int **dp, int rows)
 int ext_err(char **map, int **dp, int rows)
 {
     free_all(map, dp, rows);
-    return error_msg();
+    return err_msg();
 }
 
 int min(int a, int b, int c)
@@ -44,16 +44,18 @@ int bsq(FILE *f)
     int rows;
     int cols = 0;
     int read;
+    int best_size = 0, best_y = 0, best_x = 0;
     int **dp = NULL;
-    int best_size = 0, best_r = 0, best_x = 0;
+    size_t len = 0;
     char empty, obstacle, full;
     char **map = NULL;
-    size_t len = 0;
     if (fscanf(f, "%d %c %c %c\n", &rows, &empty, &obstacle, &full) != 4)
-        return error_msg();
+        return err_msg();
+    if (rows <= 0 || empty == obstacle || empty == full || obstacle == full)
+        return err_msg();
     map = malloc(rows * sizeof(char*));
     if (!map)
-        return error_msg();
+        return err_msg();
     for (int i = 0; i < rows; i++)
     {
         map[i] = NULL;
@@ -65,7 +67,7 @@ int bsq(FILE *f)
             map[i][read - 1] = '\0';
             read--;
         } else
-            return ext_err(map, dp, rows);       
+            return ext_err(map, dp, rows);
         if (i == 0)
             cols = read;
         else if (cols != read)
@@ -74,43 +76,43 @@ int bsq(FILE *f)
     dp = malloc(rows * sizeof(int*));
     if (!dp)
         return ext_err(map, dp, rows);
-    for (int r = 0; r < rows; r++)
+    for (int y = 0; y < rows; y++)
     {
-        dp[r] = calloc(cols, sizeof(int));
-        if (!dp[r])
+        dp[y] = calloc(cols, sizeof(int));
+        if (!dp[y])
             return ext_err(map, dp, rows);
         for (int x = 0; x < cols; x++)
         {
-            char c = map[r][x];
+            char c = map[y][x];
             if (c != empty && c != obstacle)
                 return ext_err(map, dp, rows);
             if (c == obstacle)
-                dp[r][x] = 0;
-            else if (r == 0 || x == 0)
-                dp[r][x] = 1;
+                dp[y][x] = 0;
+            else if (y == 0 || x == 0)
+                dp[y][x] = 1;
             else
-                dp[r][x] = 1 + min(dp[r - 1][x], dp[r -1][x - 1], dp[r][x - 1]);
-            if (dp[r][x] > best_size)
+                dp[y][x] = 1 + min(dp[y - 1][x], dp[y - 1][x - 1], dp[y][x - 1]);
+            if (dp[y][x] > best_size)
             {
-                best_size = dp[r][x];
-                best_r = r;
+                best_size = dp[y][x];
+                best_y = y;
                 best_x = x;
             }
         }
     }
-    for (int r = best_r - best_size + 1; r <= best_r; r++)
+    for (int y = best_y - best_size + 1; y <= best_y; y++)
     {
-        for (int x = best_x - best_size + 1; x <= best_x; x++)
-            map[r][x] = full;
+        for(int x = best_x - best_size + 1; x <= best_x; x++)
+            map[y][x] = full;
     }
-    for (int r = 0; r < rows; r++)
+    for (int y = 0; y < rows; y++)
     {
         for (int x = 0; x < cols; x++)
-            putchar(map[r][x]);
+            putchar(map[y][x]);
         putchar('\n');
     }
     free_all(map, dp, rows);
-    return 0;
+    return (0);
 }
 
 int main(int argc, char **argv)
@@ -119,19 +121,21 @@ int main(int argc, char **argv)
     int r;
     if (argc > 1)
     {
-        for(int i = 1; i < argc; i++)
+        for (int i = 1; i < argc; i++)
         {
             f = fopen(argv[i], "r");
             if (!f)
-                return error_msg();
+            {
+                err_msg();
+                continue;
+            }
             r = bsq(f);
             fclose(f);
-            if (i < argc -1)
-                fprintf(stdout, "\n");
+            if (i < argc - 1)
+                putchar('\n');
         }
     }
     else
         r = bsq(stdin);
-    
-    return (r);
+    return r;
 }
